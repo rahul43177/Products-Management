1cc33acdaa9bcaafd8e7e59338a157f8395bdb06
Rahul <rahulmishra4317@gmial.com>
2023-07-03 23:24:51 +0530

    created user schema

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const route = require('./route/route.js')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+mongoose.connect('mongodb+srv://rahul4317:L0Jf8dKS6E1sKl1C@cluster0.dwi1fgs.mongodb.net/Products-Management-DB')
+.then(()=>console.log(`MongoDB is connected`))
+.catch(error=>console.log(error))
+
+
+app.use('/',route)
+
+app.listen(5000 , function(){
+    console.log(`The server is running on the port 5000`)
+})
---
+++ src/route/route.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const checkModel = require('../model/modelchecking.js')
+
+
+
+
+module.exports = router
